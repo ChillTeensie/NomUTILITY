42826cc10e5cc16f8bb30379a5e27d4e004998a5
nomnom-dv <nomnomlmao@gmail.com>
2023-10-13 14:17:20 +0300

    Added Documentation to QuickCalls.hx

--- src/nom/QuickCalls.hx
+++ src/nom/QuickCalls.hx
@@ -11,7 +11,7 @@ import flixel.addons.display.FlxRuntimeShader;
 #end
 class QuickCalls {
 
-    #if !flash
+    #if !flas h
 
     // EXAMPLE:
     /*
@@ -31,6 +31,13 @@ class QuickCalls {
             ]
         );
     */
+        /**
+     * Create and configure a custom shader for use in your game.
+     *
+     * @param source The shader source code.
+     * @param uniformSets An array of uniform settings for the shader.
+     * @return A configured FlxRuntimeShader instance.
+     */
     public static function quickShader(source:String, uniformSets:Array<Array<Dynamic>>):FlxRuntimeShader {
         var shad = new FlxRuntimeShader(source);
     
@@ -55,6 +62,13 @@ class QuickCalls {
         return shad;
     }    
 
+    /**
+     * Create and configure a shader ported from Shadertoy for use in your game.
+     *
+     * @param source The shader source code.
+     * @param uniformSets An array of uniform settings for the shader.
+     * @return A configured FlxRuntimeShader instance.
+     */
     public static function quickPortShader(source:String, uniformSets:Array<Array<Dynamic>>):FlxRuntimeShader {
         var shad = new ShadertoyPorter(source).portShader;
     
@@ -80,6 +94,18 @@ class QuickCalls {
     }    
     #end
 
+
+    /**
+     * Create a FlxText object with optional additional parameters.
+     *
+     * @param x The x-coordinate of the text.
+     * @param y The y-coordinate of the text.
+     * @param text The text content.
+     * @param size The font size.
+     * @param width Optional width of the text.
+     * @param extraParams An array of additional parameters for the FlxText.
+     * @return A configured FlxText instance.
+     */
     public static function quickText(x:Float, y:Float, text:String, size:Int, ?width:Int = 0, ?extraParams:Array<Dynamic>):FlxText{
         var text:FlxText = new FlxText(x,y,width,text,size);
 
@@ -89,6 +115,17 @@ class QuickCalls {
         return text;
     }
 
+    /**
+     * Create and configure a FlxSprite object with additional options.
+     *
+     * @param x The x-coordinate of the sprite.
+     * @param y The y-coordinate of the sprite.
+     * @param graphicPath The path to the sprite's graphic.
+     * @param parent The parent FlxGroup to add the sprite to.
+     * @param scale An array of x and y scale values.
+     * @param scrollFactor Optional scroll factor for the sprite.
+     * @return A configured FlxSprite instance.
+     */
     public static function quickSprite(x:Float, y:Float, graphicPath:Dynamic, parent:FlxGroup, scale:Array<Float>, scrollFactor:FlxPoint = null):FlxSprite {
         var sprite:FlxSprite = new FlxSprite(x, y, graphicPath).scale.set(scale[0], scale[1]);
         if (scrollFactor != null) {
@@ -98,7 +135,20 @@ class QuickCalls {
         return sprite;
     }
 
-
+    /**
+     * Create and configure an animated FlxSprite object.
+     *
+     * @param x The x-coordinate of the sprite.
+     * @param y The y-coordinate of the sprite.
+     * @param graphicPath The path to the sprite's graphic.
+     * @param parent The parent FlxGroup to add the sprite to.
+     * @param scale An array of x and y scale values.
+     * @param frames An array of frame indices for the animation.
+     * @param frameRate The animation frame rate.
+     * @param animName The name of the animation.
+     * @param scrollFactor Optional scroll factor for the sprite.
+     * @return A configured animated FlxSprite instance.
+     */
     public static function quickAnimatedSprite(x:Float, y:Float, graphicPath:Dynamic, parent:FlxGroup, scale:Array<Float>, frames:Array<Int>, frameRate:Float, animName:String = "animation1", scrollFactor:FlxPoint = null):FlxSprite {
         var sprite:FlxSprite = new FlxSprite(x, y, graphicPath).scale.set(scale[0], scale[1]);
         sprite.animation.add(animName, frames, frameRate, true);
@@ -110,6 +160,22 @@ class QuickCalls {
         return sprite;
     }
 
+    /**
+     * Create and configure a particle emitter for particle effects.
+     *
+     * @param x The x-coordinate of the emitter.
+     * @param y The y-coordinate of the emitter.
+     * @param width The width of the emitter.
+     * @param height The height of the emitter.
+     * @param particleClass The class for custom particles (optional).
+     * @param maxParticles The maximum number of particles.
+     * @param particleLifespan The lifespan of each particle.
+     * @param autoBuffer Whether to automatically buffer particles.
+     * @param frequency The emission frequency.
+     * @param gravity Optional gravity settings.
+     * @param blend Optional blend mode.
+     * @return A configured FlxEmitter instance for particle effects.
+     */
     public static function quickParticleEmitter(
             x:Float = 0, 
             y:Float = 0, 
